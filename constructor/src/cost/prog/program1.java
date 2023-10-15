613559338de391110711430d0003d7c7d8facd33
LOHITH GOWDA C V <122782406+Lohithgowda11@users.noreply.github.com>
2023-10-15 18:05:25 +0530

    Add files via upload

---
+++ constructor/src/cost/prog/program1.java
@@ -0,0 +1,18 @@
+package cost.prog;
+
+public class program1
+{
+	program1()
+	{
+		int a= 10;
+		int b= 20;
+		System.out.println(a+b);
+		
+	}
+	public static void main(String[] args)
+	{
+		program1 p1 =new program1();
+		
+	}
+
+}
